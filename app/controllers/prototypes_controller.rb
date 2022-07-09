2240d4464e4408e649df1b410a50e22925747c8d
yui0223 <ranka1113tugumi@gmail.com>
2022-07-09 18:28:35 +0900

    編集・削除ボタンの修正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user.user_id
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
