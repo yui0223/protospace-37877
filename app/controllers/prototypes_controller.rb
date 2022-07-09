ba022ced5fb65d2a510bf92eb41a536d6ba83c5b
yui0223 <ranka1113tugumi@gmail.com>
2022-07-09 16:26:40 +0900

    プロトタイプ投稿機能の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:destroy, :edit]
 
   def index
     @prototypes = Prototype.includes(:user).order("created_at DESC")
@@ -54,7 +54,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in? && current_user.id == @prototype.user.user_id
       redirect_to action: :index
     end
   end
