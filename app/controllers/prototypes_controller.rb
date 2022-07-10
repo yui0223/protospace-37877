8296d54ef826d9d2a17a8841620cbad46c5de1b6
yui0223 <ranka1113tugumi@gmail.com>
2022-07-11 08:07:21 +0900

    編集後遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,7 @@ class PrototypesController < ApplicationController
       @prototype = Prototype.find(params[:id])
     if
       @prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
